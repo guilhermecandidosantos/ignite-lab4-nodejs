3705309f3ac39dad0f9787669b7805857b2f2f81
Guilherme <guilhermecandido1998@gmail.com>
2022-12-14 19:26:43 -0300

    create repositories/use cases

--- src/application/use-cases/send-notification.ts
+++ src/application/use-cases/send-notification.ts
@@ -1,5 +1,7 @@
+import { Injectable } from '@nestjs/common';
 import { Content } from '../entities/content';
 import { Notification } from '../entities/notification';
+import { NotificationsRepository } from '../repositories/notifications-repository';
 
 interface SendNotificationRequest {
   recipientId: string;
@@ -10,8 +12,10 @@ interface SendNotificationRequest {
 interface SendNotificationResponse {
   notification: Notification;
 }
-
+@Injectable()
 export class SendNotification {
+  constructor(private notificationsRepository: NotificationsRepository) {}
+
   async execute(
     request: SendNotificationRequest,
   ): Promise<SendNotificationResponse> {
@@ -22,6 +26,8 @@ export class SendNotification {
       content: new Content(content),
       category,
     });
+
+    await this.notificationsRepository.create(notification);
 
     return {
       notification,
